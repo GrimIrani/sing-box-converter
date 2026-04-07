10297f7e0b82c70392df5ec19df46066f7174c09
GrimIrani <GrimIrani@gmail.com>
2026-04-08 01:05:01 +0330

    docs: add docstring and reference for functions

--- singboxconverter/methods/log.py
+++ singboxconverter/methods/log.py
@@ -1,1 +1,15 @@
+"""
+We have this kind of log level inside the `sing-box`:
+- disable(return an empty `{}`)
+- trace
+- debug
+- info(Default one)
+- warn
+- error
+- fetal
+- panic
+
+depends on your choise the report of log will change
+"""
+
 pass
