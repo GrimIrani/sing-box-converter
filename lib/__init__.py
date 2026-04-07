2b6b3d2007fb3908115c4077cbc24ec2442f3e67
GrimIrani <GrimIrani@gmail.com>
2026-04-08 00:30:16 +0330

    refactor: new stracture for project

---
+++ lib/__init__.py
@@ -0,0 +1,4 @@
+from . import certificate, dns, inbounds, log, outbounds, route, tun, version
+
+__version__ = version.__version__
+__all__ = []
---
+++ lib/methods/version.py
@@ -0,0 +1,2 @@
+# More info: PEP440
+__version__ = "0.0.0"
