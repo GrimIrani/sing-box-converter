3c5a14047de3b38687507bdb57407597633d0647
GrimIrani <GrimIrani@gmail.com>
2026-04-08 17:14:06 +0330

    docs: working on the stracture

---
+++ singboxconverter/server.py
@@ -0,0 +1,9 @@
+"""
+make a sing-box server for example a ss/vless/vmess/... server using the feature of the sing-box core.
+useage:
+$sing-box-converter server --ip 0.0.0.0 --port 1080 --type socks5
+or
+$sing-box-converter server --ip 0.0.0.0 --port 1080 --vless --UUID xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+"""
+
+pass
---
+++ singboxconverter/test.py
@@ -0,0 +1,8 @@
+"""
+test the config or vless files works like this;
+$sing-box-converter test "vless://"
+or
+sbc.test("config.json", "vless://", "ss://", ...)
+
+
+"""
